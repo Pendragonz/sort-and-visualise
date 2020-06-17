438268c37b7ddb4fe0e6a7547768c893ffba82d4
parekhd <danielparekh@protonmail.com>
2020-06-17 15:43:19 +0100

    Add frame for insertion sort

---
+++ src/sorters/Insertion.java
@@ -0,0 +1,27 @@
+package sorters;
+
+/**
+ * 
+ */
+
+/**
+ * @author Pendragonz
+ *
+ */
+public class Insertion extends Sorter {
+
+	/**
+	 * @param sortable
+	 */
+	public Insertion(Sortable[] sortable) {
+		super(sortable);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Sortable[] sort() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
