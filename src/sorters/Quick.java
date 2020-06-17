3f6e1918ca488cc9af09c4f8ef019e71a624dd88
parekhd <danielparekh@protonmail.com>
2020-06-17 15:42:59 +0100

    Add frame for quick sort

---
+++ src/sorters/Quick.java
@@ -0,0 +1,16 @@
+package sorters;
+
+public class Quick extends Sorter {
+
+	public Quick(Sortable[] sortable) {
+		super(sortable);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Sortable[] sort() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
