79e4be1799eee0b533dd7d74072aebac24ab4ff0
parekhd <danielparekh@protonmail.com>
2020-06-17 15:43:36 +0100

    Add frame for merge sort

---
+++ src/sorters/Merge.java
@@ -0,0 +1,27 @@
+package sorters;
+
+/**
+ * 
+ */
+
+/**
+ * @author Pendragonz
+ *
+ */
+public class Merge extends Sorter {
+
+	/**
+	 * @param sortable
+	 */
+	public Merge(Sortable[] sortable) {
+		super(sortable);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Sortable[] sort() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
